adbeb1113c335d28705095c1a27f9199eb4e434e
angie2512 <a20191792@pucp.edu.pe>
2023-09-30 11:57:09 -0500

    primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LAB4_IOT_20191792"
+include(":app")
+ 
