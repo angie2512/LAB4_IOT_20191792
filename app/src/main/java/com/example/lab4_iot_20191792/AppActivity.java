1d6ef386cdfeaf325f50330046034f84556baad4
angie2512 <a20191792@pucp.edu.pe>
2023-10-02 01:46:52 -0500

    avancedefragments+navigation

--- app/src/main/java/com/example/lab4_iot_20191792/AppActivity.java
+++ app/src/main/java/com/example/lab4_iot_20191792/AppActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.lab4_iot_20191792.databinding.ActivityMainBinding;
+import com.example.lab4_iot_20191792.fragments.AcelerometroFragment;
+
 public class AppActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmentContainerView, AcelerometroFragment.class, null)
+                    .commit();
+        }
     }
 }
